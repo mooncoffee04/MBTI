5c606388553293a2699ae1cb8edc93374617af7b
Laavanya Mishra <165663784+mooncoffee04@users.noreply.github.com>
2025-03-30 04:57:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT variable
+    port = int(os.environ.get("PORT", 10000))  # Use Render's PORT variable
     app.run(host="0.0.0.0", port=port, debug=True)
